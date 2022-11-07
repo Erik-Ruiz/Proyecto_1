e7ecac340a2fdc99163e35a0828a1da0c6f9b14b
Jordi <jordi.cort02@gmail.com>
2022-11-07 17:25:32 +0100

    Login hecho

--- index.php
+++ index.php
@@ -1,1 +1,51 @@
-<script>location.href='./pages/info.php'</script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- ===== Iconscout CSS ===== -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+    <!-- ===== CSS ===== -->
+    <link rel="stylesheet" href="./css/login.css">
+
+    <script src="./js/login.js"></script>
+
+    <title>Login</title>
+</head>
+
+<body>
+    
+    <div class="container">
+        <div class="forms">
+            <div class="form login">
+                <span class="title">Login</span>
+
+                <form action="#">
+                    <div class="input-field">
+                        <input type="text" placeholder="Enter your email" required>
+                        <i class="uil uil-envelope icon"></i>
+                    </div>
+                    <div class="input-field">
+                        <input type="password" class="password" placeholder="Enter your password" required>
+                        <i class="uil uil-lock icon"></i>
+                    </div>
+
+                    <div class="input-field button">
+                        <input type="button" value="Login">
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
+
+
+
+</body>
+</html>
