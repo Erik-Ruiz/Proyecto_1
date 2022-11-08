040d7d4783cbdb0f819f2d897ef515f8f0fc0281
erik <erikrm02@gmail.com>
2022-11-08 20:38:15 +0100

    last update

--- includes/validacion.php
+++ includes/validacion.php
@@ -1,4 +1,5 @@
 <?php
+
 function errorEmail($email){
     
     if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
@@ -6,43 +7,13 @@ function errorEmail($email){
     }else{
         $error=false;
     }
-
     return $error;
- 
+    
 }
-
-function comprobar_email($email){
-    $mail_correcto = 0;
-    //compruebo unas cosas primeras
-    if ((strlen($email) >= 6) && (substr_count($email,"@") == 1) && (substr($email,0,1) != "@") && (substr($email,strlen($email)-1,1) != "@")){
-       if ((!strstr($email,"'")) && (!strstr($email,"\"")) && (!strstr($email,"\\")) && (!strstr($email,"\$")) && (!strstr($email," "))) {
-          //miro si tiene caracter .
-          if (substr_count($email,".")>= 1){
-             //obtengo la terminacion del dominio
-             $term_dom = substr(strrchr ($email, '.'),1);
-             //compruebo que la terminación del dominio sea correcta
-             if (strlen($term_dom)>1 && strlen($term_dom)<5 && (!strstr($term_dom,"@")) ){
-                //compruebo que lo de antes del dominio sea correcto
-                $antes_dom = substr($email,0,strlen($email) - strlen($term_dom) - 1);
-                $caracter_ult = substr($antes_dom,strlen($antes_dom)-1,1);
-                if ($caracter_ult != "@" && $caracter_ult != "."){
-                   $mail_correcto = 1;
-                }
-             }
-          }
-       }
-    }
-    if ($mail_correcto)
-       return 1;
-    else
-       return 0;
- }
-
 function errorPsswd($pass){
-    if (preg_match_all('$S*(?=S{8,})S*$', $password) == true) {
+    if (preg_match_all('$S*(?=S{8,})S*$', $pass) == true) {
         echo "Correcto";
     } else {
         echo "Incorrecto";
     }
 }
-
