68cc52d993a59f70842c7a38b2360fcfb2c2ab99
Jordi <jordi.cort02@gmail.com>
2022-11-10 17:45:00 +0100

    Push hora.php

--- controller/hora.php
+++ controller/hora.php
@@ -4,7 +4,7 @@ function tiempoinicio(){
     require ("./connection.php");
 
     $DateAndTime = date('m-d-Y h:i:s', time());  
-    echo "The current date and time are $DateAndTime";
+    // echo "The current date and time are $DateAndTime";
     
     $sql1="INSERT INTO tbl_t_comer (t_i_comer,t_f_comer,id_mesa) VALUES ('$DateAndTime','',2)";
     $stmt=mysqli_stmt_init($conexion);
@@ -12,27 +12,39 @@ function tiempoinicio(){
     
     if (mysqli_stmt_execute($stmt) === TRUE) {
         $last_id = $conexion->insert_id;
-        echo "New record created successfully. Last inserted ID is: " . $last_id;
+        // echo "New record created successfully. Last inserted ID is: " . $last_id;
     } else {
         echo "Error: " . $sql1 . "<br>" . $conexion->error;
     }
     
     mysqli_stmt_close($stmt);
 
+
     return $last_id;
 }
 echo tiempoinicio();
 
 
-// function tiempofin($last_id){
-//     $stmt=$pdo->prepare("SELECT id FROM tbl_t_comer WHERE t_i_comer='',id=''");
-//     $stmt->bindParam(':nombre', $alu->nombre);
 
-//     $DateAndTime2 = date('m-d-Y h:i:s', time());
-//     $ka="SELECT id from tbl_t_comer WHERE ('','$DateAndTime','',2)";  
-//     $sql4="SELECT id_comer from tbl_t_comer WHERE ('','$DateAndTime','',2)";  
-//     $sql=("UPDATE INTO tbl_tiempo (id,tiempo_inicio,tiempo_fin) VALUES ('$id2',NULL,'$DateAndTime2')");
-// }
+function tiempofin($last_id){
+    
+    require ("./connection.php");
+
+    $DateAndTimeF = date('m-d-Y h:i:s', time());  
+    // echo "The current date and time are $DateAndTime";
+
+    $t_f_comer = "UPDATE tbl_t_comer (t_i_comer,t_f_comer,id_mesa) VALUES ('','$DateAndTimeF',2)";
+
+
+    
+    // $stmt=$pdo->prepare("SELECT id FROM tbl_t_comer WHERE t_i_comer='',id=''");
+    // $stmt->bindParam(':nombre', $alu->nombre);
+
+    // $DateAndTime2 = date('m-d-Y h:i:s', time());
+    // $ka="SELECT id from tbl_t_comer WHERE ('','$DateAndTime','',2)";  
+    // $sql4="SELECT id_comer from tbl_t_comer WHERE ('','$DateAndTime','',2)";  
+    // $sql=("UPDATE INTO tbl_tiempo (id,tiempo_inicio,tiempo_fin) VALUES ('$id2',NULL,'$DateAndTime2')");
+}
 
 // function concat(){
 // $DateAndTime3 ="SELECT id,tiempo_inicio,tiempo_fin,CONCAT(FLOOR(hours_part / 3600), ' hours ',FLOOR(minutes_part / 60), ' minutes ',seconds_part, ' seconds') AS diferencia FROM tbl_tiempo";
